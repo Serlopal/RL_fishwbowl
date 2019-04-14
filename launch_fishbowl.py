c0193bdf9ef90cdc4591420a40a17e18da6eb13b
Serlopal <oro_sergio@hotmail.com>
2019-04-14 13:20:47 +0100

    OOP overhaul. now player and enemies are instances of classes and the whole program works using OOP. also a new label counts the number of games, as when all enemies are dead the game restarts. This means that the overall goal has change from the player having to dodge enemies to the player having to touch all of them in the shortest time possible. Dead enemies don't move anymore and change color until the end of the game

--- launch_fishbowl.py
+++ launch_fishbowl.py
@@ -6,155 +6,56 @@ import time
 import numpy as np
 import threading
 
-class SignalEmitter(QWidget):
-	valueChanged = pyqtSignal(object)
 
-	def __init__(self):
+class DynamicLabel(QLabel):
+	signal = pyqtSignal(object)
+
+	def __init__(self, base_text):
 		super().__init__()
-		self._value = 0
+		self.font = QFont("Times", 30, QFont.Bold)
+		self.setFont(self.font)
 
-	@property
-	def value(self):
-		return self._t
-
-	@value.setter
-	def value(self, value):
-		self._value = value
-		self.valueChanged.emit(value)
+		self.base_text = base_text
+		self.setText(self.base_text + "0")
+		self.signal.connect(lambda x: self.setText(self.base_text + x))
 
 
-class MushroomWidget(QWidget):
-	def __init__(self):
-		super().__init__()
+class NPC():
+	allowed_dirs = np.array([[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]])
 
-		self.origin = np.array([0, 0])
-		self.nenemies = 10
-		self.nplayers = 1
+	def __init__(self, diameter, fishbowl_diameter):
+		self.d = diameter / fishbowl_diameter
+		self.original_color = Qt.black
+		self.color = Qt.black
+		self.coords = np.array([0, 0])
+		self.v = np.array([0, 0])
+		self.first = True
+		self.dead = False
 
-		self.balls_coords_emitter = SignalEmitter()
-		self.internal_balls_coords = np.zeros((self.nenemies + self.nplayers, 2))
-		self.enemies_color = Qt.red
+	def move(self):
+		if not self.dead:
+			self.coords = self.coords + self.v
+			if not self.inside_sphere(self.coords) or self.first:
+				self.first = False
+				self.v = np.random.randint(low=-100, high=100, size=2) * 0.00002
 
-		self.players_color = Qt.blue
+				self.coords = self.spherical_clip(self.coords)
 
-		self.origin_color = Qt.black
-		self.mushroom_color = Qt.black
+	def killed_by(self, player):
+		p = player.coords
+		e = self.coords
+		dist= np.sqrt((e[0] - p[0]) ** 2 + (e[1] - p[1]) ** 2)
+		if dist < self.d:
+			self.dead = True
+			self.color = Qt.gray
 
-		self.mushroom_size_origin = 350
-		self.mushroom_size = self.mushroom_size_origin
-		self.wheel_size = self.mushroom_size_origin * 1.15
-		self.wheel_width = self.mushroom_size_origin * 0.15
-		self.cursor_size = self.mushroom_size_origin * 0.05
-		self.mushroom_border_size = self.mushroom_size_origin * 0.004
-		self.mushroom_thin_border_size = self.mushroom_size_origin * 0.002
+	def revive(self):
+		self.color = self.original_color
+		self.coords = np.array([0, 0])
+		self.v = np.array([0, 0])
+		self.first = True
+		self.dead = False
 
-
-		self.coords_emitter = SignalEmitter()
-		# connect signal from emitter to change value of button
-		self.balls_coords_emitter.valueChanged.connect(lambda value: self.setCoords(value))
-
-	def scale_point(self, point):
-		original_max = 0.5
-		new_max = self.mushroom_size
-		return ((p / original_max) * new_max for p in point)
-
-	def setCoords(self, value):
-		self.internal_balls_coords = value
-		self.repaint()
-
-	def setTap(self, tap):
-		if tap:
-			self.mushroom_size = self.mushroom_size * 0.9
-		else:
-			self.mushroom_size = self.mushroom_size_origin
-		self.repaint()
-
-	def paintEvent(self, e):
-		qp = QPainter()
-		qp.begin(self)
-		self.drawWidget(qp)
-		qp.end()
-
-	def drawWidget(self, qp):
-		c = self.rect().center()
-		c_coords = c.x(), c.y()
-		background_color = self.palette().color(QPalette.Background)
-
-		# paint inner trackpad
-		qp.setPen(QPen(self.mushroom_color, self.mushroom_border_size, Qt.SolidLine))
-		# draw mushroom
-		qp.setBrush(QBrush(Qt.gray, Qt.SolidPattern))
-		qp.drawEllipse(c, *([self.mushroom_size]*2))
-
-		# draw axis lines
-		qp.setPen(QPen(self.mushroom_color, self.mushroom_thin_border_size, Qt.DashDotDotLine))
-		for angle in range(0, 420, 45):
-			line = QLineF(); line.setP1(c); line.setAngle(angle); line.setLength(self.mushroom_size)
-			qp.drawLine(line)
-		# draw wheel separators
-		line = QLineF(); line.setP1(c + QPoint(self.wheel_size, 0)); line.setAngle(0); line.setLength(self.wheel_width);qp.drawLine(line)
-		line = QLineF(); line.setP1(c + QPoint(0, -self.wheel_size)); line.setAngle(90); line.setLength(self.wheel_width);qp.drawLine(line)
-		line = QLineF(); line.setP1(c + QPoint(-self.wheel_size, 0)); line.setAngle(180); line.setLength(self.wheel_width);qp.drawLine(line)
-		line = QLineF(); line.setP1(c + QPoint(0, self.wheel_size)); line.setAngle(270); line.setLength(self.wheel_width);qp.drawLine(line)
-
-		# draw enemies
-		for i, ball in enumerate(self.internal_balls_coords):
-			qp.setPen(QPen(self.mushroom_color, self.mushroom_border_size, Qt.SolidLine))
-			if i < self.nenemies:
-				qp.setBrush(QBrush(self.enemies_color, Qt.SolidPattern))
-			else:
-				qp.setBrush(QBrush(self.players_color, Qt.SolidPattern))
-
-			qp.drawEllipse(c + QPoint(*self.scale_point(ball)), *([self.cursor_size] * 2))
-
-	def animate_balls(self):
-		self.update_thread = threading.Thread(target=self._animate_balls)
-		self.update_thread.daemon = True
-		self.update_thread.start()
-
-	def _animate_balls(self):
-		nballs = self.nenemies + self.nplayers
-		balls = np.zeros((nballs, 2))
-		v_prev_dir = np.empty((nballs, 2))
-		v_dir = np.empty((nballs , 2))
-		v_speed = np.empty((nballs, 2))
-		v = np.zeros((nballs, 2))
-		first = np.ones((nballs, 1))
-
-		while True:
-			for i in range(len(balls)):
-				balls[i] += v[i]
-				if not self.inside_sphere(balls[i]) or first[i]:
-					first[i] = 0
-					while True:
-						v_dir[i] = np.random.choice([1, 0, -1], size=2, replace=True)
-						if np.any(v_dir[i] != v_prev_dir[i]) and np.any(v_dir[i]):
-							v_prev_dir[i] = v_dir[i]
-							break
-					v_speed[i] = np.random.randint(low=10, high=20, size=2) * 0.0001
-					v[i] = np.multiply(v_speed[i], v_dir[i])  # np.random.randint(low=10, high=20) * 0.0001
-
-					balls[i] = self.spherical_clip(balls[i])
-			time.sleep(0.01)
-			self.balls_coords_emitter.value = balls
-
-	def check_collisions(self):
-		self.collision_thread = threading.Thread(target=self._check_collision)
-		self.collision_thread.daemon = True
-		self.collision_thread.start()
-
-	def _check_collision(self):
-		time.sleep(2)
-		enemies = self.internal_balls_coords[0:self.nenemies]
-		players = self.internal_balls_coords[self.nenemies:]
-
-		while True:
-			for p in players:
-				for e in enemies:
-					dist, d = np.sqrt((e[0] - p[0]) ** 2 + (e[1] - p[1]) ** 2) , (self.cursor_size / self.mushroom_size)
-					if dist < d:
-						print("DEAD")
-						break
 	@staticmethod
 	def spherical_clip(p, r=0.47):
 		p = np.array(p)
@@ -170,6 +71,133 @@ class MushroomWidget(QWidget):
 			return False
 		else:
 			return True
+
+
+class Enemy(NPC):
+	def __init__(self, diameter, fishbowl_diameter):
+		super().__init__(diameter, fishbowl_diameter)
+		self.original_color = Qt.red
+		self.color = Qt.red
+
+
+class Player(NPC):
+	def __init__(self, diameter, fishbowl_diameter):
+		super().__init__(diameter, fishbowl_diameter)
+		self.original_color = Qt.blue
+		self.color = Qt.blue
+
+
+class Fishbowl(QWidget):
+	animation_emitter = pyqtSignal(object)
+
+	def __init__(self, n_games_signal):
+		super().__init__()
+
+		# connect signal from emitter to trigger the animation
+		self.animation_emitter.connect(lambda x: self._move_npcs(x))
+
+		self.fishbowl_color = Qt.black
+
+		self.fishbowl_size = 350
+		self.wheel_size = self.fishbowl_size * 1.15
+		self.wheel_width = self.fishbowl_size * 0.15
+		self.npc_size = self.fishbowl_size * 0.05
+		self.fishbowl_border_size = self.fishbowl_size * 0.004
+		self.fishbowl_thin_border_size = self.fishbowl_size * 0.002
+
+		self.nenemies = 10
+		self.enemies = [Enemy(self.npc_size, self.fishbowl_size) for _ in range(self.nenemies)]
+		self.player = Player(self.npc_size, self.fishbowl_size)
+
+		self.start_flag = True
+		self.start_time = None
+		self.time_advantage = 2
+		self.n_games = 0
+		self.n_games_signal = n_games_signal
+
+	def scale_point(self, point):
+		original_max = 0.5
+		new_max = self.fishbowl_size
+		return ((p / original_max) * new_max for p in point)
+
+	def _move_npcs(self, command):
+		# update position of enemies and player and check dead enemies
+		if self.start_flag:
+			self.start_flag = False
+			self.start_time = time.time()
+		go_player = time.time() - self.start_time > self.time_advantage
+		alive_enemies = [x for x in self.enemies if not x.dead]
+		if not alive_enemies:
+			self.restart_game()
+			return
+		for enemy in alive_enemies:
+			enemy.move()
+			# check dead
+			if go_player:
+				enemy.killed_by(self.player)
+		if go_player:
+			self.player.move()
+
+		self.repaint()
+
+	def paintEvent(self, e):
+		qp = QPainter()
+		qp.begin(self)
+		self.drawWidget(qp)
+		qp.end()
+
+	def drawWidget(self, qp):
+		c = self.rect().center()
+		c_coords = c.x(), c.y()
+		background_color = self.palette().color(QPalette.Background)
+
+		# paint inner trackpad
+		qp.setPen(QPen(self.fishbowl_color, self.fishbowl_border_size, Qt.SolidLine))
+		# draw fishbowl
+		qp.setBrush(QBrush(Qt.gray, Qt.SolidPattern))
+		qp.drawEllipse(c, *([self.fishbowl_size] * 2))
+
+		# draw axis lines
+		qp.setPen(QPen(self.fishbowl_color, self.fishbowl_thin_border_size, Qt.DashDotDotLine))
+		for angle in range(0, 420, 45):
+			line = QLineF(); line.setP1(c); line.setAngle(angle); line.setLength(self.fishbowl_size)
+			qp.drawLine(line)
+		# draw wheel separators
+		line = QLineF(); line.setP1(c + QPoint(self.wheel_size, 0)); line.setAngle(0); line.setLength(self.wheel_width)
+		qp.drawLine(line)
+		line = QLineF(); line.setP1(c + QPoint(0, -self.wheel_size)); line.setAngle(90); line.setLength(self.wheel_width)
+		qp.drawLine(line)
+		line = QLineF(); line.setP1(c + QPoint(-self.wheel_size, 0)); line.setAngle(180); line.setLength(self.wheel_width)
+		qp.drawLine(line)
+		line = QLineF(); line.setP1(c + QPoint(0, self.wheel_size)); line.setAngle(270); line.setLength(self.wheel_width)
+		qp.drawLine(line)
+
+		qp.setPen(QPen(self.fishbowl_color, self.fishbowl_border_size, Qt.SolidLine))
+		# draw enemies
+		for i, enemy in enumerate(self.enemies):
+			qp.setBrush(QBrush(enemy.color, Qt.SolidPattern))
+			qp.drawEllipse(c + QPoint(*self.scale_point(enemy.coords)), *([self.npc_size] * 2))
+		# draw player
+		qp.setBrush(QBrush(self.player.color, Qt.SolidPattern))
+		qp.drawEllipse(c + QPoint(*self.scale_point(self.player.coords)), *([self.npc_size] * 2))
+
+	def animate_balls(self):
+		self.update_thread = threading.Thread(target=self._animate_balls)
+		self.update_thread.daemon = True
+		self.update_thread.start()
+
+	def _animate_balls(self):
+		while True:
+			time.sleep(0.01)
+			self.animation_emitter.emit("animate")
+
+	def restart_game(self):
+		self.n_games += 1
+		self.n_games_signal.emit(str(self.n_games))
+		self.start_flag = True
+		for enemy in self.enemies:
+			enemy.revive()
+		self.player.revive()
 
 
 class gameUI:
@@ -188,47 +216,38 @@ class gameUI:
 		self.window.setCentralWidget(self.main_group)
 		# set window geometry
 		ag = QDesktopWidget().availableGeometry()
-		self.window.move(ag.width()*0.15, ag.height()*0.05)
-		self.window.setMinimumWidth(ag.width()*0.3)
-		self.window.setMinimumHeight(ag.height()*0.4)
+		self.window.move(int(ag.width()*0.15), int(ag.height()*0.05))
+		self.window.setMinimumWidth(int(ag.width()*0.3))
+		self.window.setMinimumHeight(int(ag.height()*0.4))
 
-		self.layout =QVBoxLayout()
-		self.create_mushroom_group()
-		self.layout.addWidget(self.mushroom_group)
+		self.layout = QGridLayout()
+		self.n_games_label = DynamicLabel("Game ")
+		self.layout.addWidget(self.n_games_label, 0,0,1,10)
+		self.fishbowl = Fishbowl(self.n_games_label.signal)
+		self.layout.addWidget(self.fishbowl, 1, 0, 10, 10)
+
 		self.main_group.setLayout(self.layout)
 
 		# set layout inside window
 		self.window.setLayout(self.layout)
 		self.window.show()
 
+	def start_ui(self):
+		"""
+		starts the ball animation thread and launches the QT app
+		"""
 		self.start_animation()
-
-	def create_mushroom_group(self):
-		# create group
-		self.mushroom_group = QGroupBox("Mushroom")
-		# create widgets
-		self.mushroom = MushroomWidget()
-		# create group layout
-		layout = QVBoxLayout()
-		layout.addWidget(self.mushroom)
-		# add layout to group
-		self.mushroom_group.setLayout(layout)
-
-	def start_UI(self):
 		self.app.exec()
 
 	def start_animation(self):
+		"""
+		waits 1 second so that the QT app is running and then launches the ball animation thread
+		"""
 		time.sleep(1)
-		self.mushroom.animate_balls()
-		self.mushroom.check_collisions()
-
-
-
-
-
+		self.fishbowl.animate_balls()
 
 
 if __name__ == "__main__":
 
 	ui = gameUI()
-	ui.start_UI()
+	ui.start_ui()
